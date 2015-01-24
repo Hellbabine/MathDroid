4aeb7f5f878053d866bf68da6fb4e15e9e24b0ea
Samuel <samgacha@hotmail.com>
2015-01-24 06:09:14 -0800

    Ajout du code a l'interface pour le faire fonctionner, sauf resoudre

--- app/src/main/java/com/example/hellbabine/mathdroid/MainActivity.java
+++ app/src/main/java/com/example/hellbabine/mathdroid/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.hellbabine.mathdroid;
 
+import android.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private TextView txtEquation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("Lancement de l'app");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtEquation = (TextView) findViewById(R.id.txtEquation);
     }
 
 
@@ -35,5 +42,120 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void btn1Click(View v)
+    {
+        AddCarToEquation("1");
+    }
+
+    public void btn2Click(View v)
+    {
+        AddCarToEquation("2");
+    }
+
+    public void btn3Click(View v)
+    {
+        AddCarToEquation("3");
+    }
+
+    public void btn4Click(View v)
+    {
+        AddCarToEquation("4");
+    }
+
+    public void btn5Click(View v)
+    {
+        AddCarToEquation("5");
+    }
+
+    public void btn6Click(View v)
+    {
+        AddCarToEquation("6");
+    }
+
+    public void btn7Click(View v)
+    {
+        AddCarToEquation("7");
+    }
+
+    public void btn8Click(View v)
+    {
+        AddCarToEquation("8");
+    }
+
+    public void btn9Click(View v)
+    {
+        AddCarToEquation("9");
+    }
+
+    public void btn0Click(View v)
+    {
+        AddCarToEquation("0");
+    }
+
+    public void btnPlusClick(View v)
+    {
+        AddCarToEquation("+");
+    }
+
+    public void btnMinusClick(View v)
+    {
+        AddCarToEquation("-");
+    }
+
+    public void btnDivideClick(View v)
+    {
+        AddCarToEquation("/");
+    }
+
+    public void btnMultiplyClick(View v)
+    {
+        AddCarToEquation("*");
+    }
+
+    public void btnEqualClick(View v)
+    {
+        AddCarToEquation("=");
+    }
+
+    public void btnComaClick(View v)
+    {
+        AddCarToEquation(".");
+    }
+
+    public void btnPowerClick(View v)
+    {
+        AddCarToEquation("^");
+    }
+
+    public void btnVarClick(View v)
+    {
+        AddCarToEquation("X");
+    }
+
+    public void btnOpenClick(View v)
+    {
+        AddCarToEquation("(");
+    }
+
+    public void btnCloseClick(View v)
+    {
+        AddCarToEquation(")");
+    }
+
+    public void btnEraseClick(View v)
+    {
+        if(txtEquation.getText().length() != 0)
+        {
+            txtEquation.setText(txtEquation.getText().subSequence(0, txtEquation.getText().length() -1));
+        }
+    }
+
+
+
+    private void AddCarToEquation(String c)
+    {
+        txtEquation.setText(txtEquation.getText() + c);
     }
 }
